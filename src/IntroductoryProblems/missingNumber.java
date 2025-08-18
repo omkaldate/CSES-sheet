e8bfb4f2476875e29fdcf4eec3c7e015bfe99dd5
Om Kaldate <omkaldate23@gmail.com>
2025-08-18 21:20:18 +0530

    cses basic problems

---
+++ src/IntroductoryProblems/missingNumber.java
@@ -0,0 +1,89 @@
+package IntroductoryProblems;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class missingNumber {
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next(){
+            while(st==null || !st.hasMoreTokens()){
+                try {
+                    st=new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        double nextDouble(){
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str="";
+            try {
+                str=br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    static class FastWriter {
+        private final BufferedWriter bw;
+
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            FastReader in = new FastReader();
+            FastWriter out = new FastWriter();
+            //
+            int n = in.nextInt();
+            int[]arr = new int[n+1];
+
+            for(int i=0; i<n-1; i++){
+               int number = in.nextInt();
+               arr[number] = number;
+            }
+
+            for(int i=1; i<= n; i++){
+                if(arr[i] != i){
+                    out.print(i);
+                }
+            }
+
+
+            out.close();
+        } catch (Exception e) {
+            return;
+        }
+    }
+}
